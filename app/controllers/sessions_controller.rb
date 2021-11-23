a8ec41b5e697dfd63e48995a19d854973ae8668e
huyen <18020663@vnu.edu.vn>
2021-11-23 21:06:08 +0700

    session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,20 @@
 class SessionsController < ApplicationController
+    def new
+    end
+    
+    def create
+        user = User.find_by(email: param[:email])
+        if user.present? && user.authenticate(param[:password])
+            session[:user_email] = user.email
+            redirect_to root_path, notice: "Logged in successfully"
+        else
+            flash[:alert] = "Invalid email or password"
+            render :new
+        end
+    end
+
+    def destroy 
+        session[:user_email] = nil
+        redirect_to root_path, notice: "Logged out"
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root "static_pages#home"
   get "log_in", to: "sessions#new"
+  post "log_in", to: "sessions#create"
 end
