5ade26fc9dd0469d6bff03e1b1efb395d212f3aa
nhat.ns <nhat.ns@teko.vn>
2021-11-23 20:12:09 +0700

    validate password

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
   enum role: { user: 0, admin: 1 }, _prefix: :role
 
   validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address" }
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 8, maximum: 256 }
 end
