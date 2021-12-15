577af16c93406f875f4ca4403122acf99abf1d74
Tran Nguyen <trannguyen61st@gmail.com>
2021-12-15 09:56:29 +0700

    fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
         "#{key}" if current_page? path
         # We use string interpolation "#{key}" here to access the CSS classes we are going to create.
      end
+
+    def is_admin?
+        session[:user_role] == "admin"
+    end
 end
