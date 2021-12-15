7346b744231f3375942feae2dc8aa6d6bcdc6067
khang <43407475+khangneko@users.noreply.github.com>
2021-12-15 08:12:33 +0700

    Adding admin nav bar (#18)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    def current?(key, path)
+        puts key
+        puts current_page?
+        puts path
+        "#{key}" if current_page? path
+        # We use string interpolation "#{key}" here to access the CSS classes we are going to create.
+     end
 end
